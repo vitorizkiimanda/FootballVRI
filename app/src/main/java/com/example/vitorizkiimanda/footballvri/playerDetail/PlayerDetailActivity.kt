419ccd988cce42c90e48b3339f87a6013e938612
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-12-16 01:39:07 +0700

    Player Detail : Receiving data intent

--- app/src/main/java/com/example/vitorizkiimanda/footballvri/playerDetail/PlayerDetailActivity.kt
+++ app/src/main/java/com/example/vitorizkiimanda/footballvri/playerDetail/PlayerDetailActivity.kt
@@ -2,6 +2,8 @@ package com.example.vitorizkiimanda.footballvri.playerDetail
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.vitorizkiimanda.footballvri.Model.Match
+import com.example.vitorizkiimanda.footballvri.Model.Player
 import com.example.vitorizkiimanda.footballvri.R
 
 class PlayerDetailActivity : AppCompatActivity() {
@@ -9,5 +11,11 @@ class PlayerDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_player_detail)
+
+        val data: Player = intent.getParcelableExtra<Player>("dataPlayer")
+
+        //Toolbar
+        supportActionBar?.title = data.strPlayer
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
