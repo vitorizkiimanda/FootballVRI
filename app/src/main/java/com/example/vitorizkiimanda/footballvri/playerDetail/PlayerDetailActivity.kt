8733c75bca6ee4fbfbc67050642c1dd399d0d2a3
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-12-16 02:05:47 +0700

    Player Detail : Done

--- app/src/main/java/com/example/vitorizkiimanda/footballvri/playerDetail/PlayerDetailActivity.kt
+++ app/src/main/java/com/example/vitorizkiimanda/footballvri/playerDetail/PlayerDetailActivity.kt
@@ -2,9 +2,13 @@ package com.example.vitorizkiimanda.footballvri.playerDetail
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.vitorizkiimanda.footballvri.Model.Match
 import com.example.vitorizkiimanda.footballvri.Model.Player
 import com.example.vitorizkiimanda.footballvri.R
+import kotlinx.android.synthetic.main.activity_player_detail.*
 
 class PlayerDetailActivity : AppCompatActivity() {
 
@@ -17,5 +21,26 @@ class PlayerDetailActivity : AppCompatActivity() {
         //Toolbar
         supportActionBar?.title = data.strPlayer
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        //setData to UI
+        Glide.with(applicationContext)
+                .load(data.strThumb)
+                .apply(RequestOptions().placeholder(R.mipmap.ic_launcher))
+                .into(player_thumb)
+        player_weight.text = data.strWeight
+        player_height.text = data.strHeight
+        player_posisition.text = data.strPosition
+        player_description.text = data.strDescriptionEN
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
