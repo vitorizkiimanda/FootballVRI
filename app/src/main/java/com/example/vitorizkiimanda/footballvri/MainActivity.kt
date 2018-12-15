bfbbf35a146430b07062c4afa6d263513e264180
Vito Rizki Imanda <vitorizkiimanda@gmail.com>
2018-12-15 17:44:29 +0700

    UI : Basic Fragments

--- app/src/main/java/com/example/vitorizkiimanda/footballvri/MainActivity.kt
+++ app/src/main/java/com/example/vitorizkiimanda/footballvri/MainActivity.kt
@@ -3,32 +3,58 @@ package com.example.vitorizkiimanda.footballvri
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import com.example.vitorizkiimanda.footballvri.R.id.*
+import com.example.vitorizkiimanda.footballvri.favourites.FavouritesFragment
+import com.example.vitorizkiimanda.footballvri.matches.MatchesFragment
+import com.example.vitorizkiimanda.footballvri.teams.TeamsFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_notifications)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                navigation_matches -> {
+                    loadMatchesFragment(savedInstanceState)
+                }
+                navigation_teams -> {
+                    loadTeamsFragment(savedInstanceState)
+                }
+                navigation_favourites -> {
+                    loadFavouritesFragment(savedInstanceState)
+                }
+            }
+            true
+        }
+        navigation.selectedItemId = navigation_matches
+    }
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    private fun loadMatchesFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, MatchesFragment(), MatchesFragment::class.simpleName)
+                    .commit()
+        }
+    }
+
+    private fun loadTeamsFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, TeamsFragment(), TeamsFragment::class.simpleName)
+                    .commit()
+        }
+    }
+
+    private fun loadFavouritesFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.main_container, FavouritesFragment(), FavouritesFragment::class.simpleName)
+                    .commit()
+        }
     }
 }
